0d25c15dbd1ce471f86e29bf7db61f824d4eccb8
RNatvik <r_bn-95@hotmail.com>
2019-09-17 13:06:26 +0200

    Added functionality to paint the detected circle as output image.

--- src/main/java/ImageConsumer.java
+++ src/main/java/ImageConsumer.java
@@ -1,5 +1,4 @@
 import org.bytedeco.javacpp.indexer.UByteBufferIndexer;
-import org.bytedeco.javacpp.indexer.UByteRawIndexer;
 import org.bytedeco.javacv.CanvasFrame;
 import org.bytedeco.javacv.Frame;
 
@@ -35,7 +34,7 @@ public class ImageConsumer implements Runnable {
     }
 
     public void setup() {
-        this.thresholder.setFirstRange(69, 91, 97, 255, 0, 255);
+        this.thresholder.setFirstRange(33, 80, 15, 155, 53, 200);
         this.thresholder.setSecondRange(0, 40, 200, 255, 100, 255);
         this.thread.start();
     }
@@ -58,33 +57,39 @@ public class ImageConsumer implements Runnable {
                     this.canvas6.setCanvasSize(frame.imageWidth, frame.imageHeight);
                     this.firstImage = false;
                 }
-                Frame blurredFrame = this.thresholder.blurFilter(frame, 7);
+                Frame blurredFrame = this.thresholder.blurFilter(frame, 9);
                 Frame thresholdFrame = this.thresholder.thresholdFirstRange(blurredFrame);
-                Frame medFrame = this.thresholder.medFilter(thresholdFrame, 7);
-                Frame erodedFrame = this.thresholder.erodeImage(medFrame, 5);
-                Frame dilatedFrame = this.thresholder.dilateImage(erodedFrame, 5);
-                List<int[]> locations = this.thresholder.customDetectCircle(dilatedFrame,20, 30, 10, 0.4);
+                Frame medFrame = this.thresholder.medFilter(thresholdFrame, 5);
+                Frame erodedFrame = this.thresholder.erodeImage(medFrame, 3);
+                Frame dilatedFrame = this.thresholder.dilateImage(erodedFrame, 3);
+                List<int[]> locations = this.thresholder.customDetectCircle(dilatedFrame, 10, 50, 10, 0.4);
+
                 Frame paintedFrame = frame.clone();
-                /* TODO: find a way to paint circles on top of captured image safely
                 UByteBufferIndexer indexer = paintedFrame.createIndexer();
                 for (int[] location : locations) {
                     int xCenter = location[0];
                     int yCenter = location[1];
                     int r = location[2];
-                    int numSteps = (int) (2*Math.PI*r);
-                    for (double step = 0; step < numSteps + 1; step++) {
-                        int x = r*(int)Math.cos(step / r);
-                        int y = r*(int)Math.sin(step / r);
+                    for (int i = r; i < r + 2; i++) {
+                        int numSteps = (int) (2 * Math.PI * i);
+                        for (double step = 0; step < numSteps; step++) {
+                            int x = xCenter + (int) (i * Math.cos(step * 2 * Math.PI / numSteps));
+                            int y = yCenter + (int) (i * Math.sin(step * 2 * Math.PI / numSteps));
 
-                        indexer.put(y + yCenter, x + xCenter, 255, 1);
+                            try {
+                                indexer.put(y, x, new int[]{0, 0, 255});
+                            } catch (IndexOutOfBoundsException iob) {
+                                System.out.println("Tried to paint out of frame");
+                            }
+                        }
                     }
-                }*/
-                this.canvas.showImage(frame);
+                }
+                this.canvas.showImage(dilatedFrame);
 //                this.canvas2.showImage(blurredFrame);
 //                this.canvas3.showImage(medFrame);
 //                this.canvas4.showImage(thresholdFrame);
 //                this.canvas5.showImage(erodedFrame);
-                this.canvas6.showImage(dilatedFrame);
+                this.canvas6.showImage(paintedFrame);
                 System.out.println(System.currentTimeMillis() - startTime);
             }
         }
