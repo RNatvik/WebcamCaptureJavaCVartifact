a5f7c2b9fb41e3f3a10c129a3e571cfca7ec0cd6
RNatvik <r_bn-95@hotmail.com>
2019-09-28 17:14:48 +0200

    Added getters in Image and PidParameter

--- src/main/java/data/PidParameter.java
+++ src/main/java/data/PidParameter.java
@@ -6,10 +6,22 @@ public class PidParameter extends Data {
     private int ki;
     private int kd;
 
-    public PidParameter(int kp, int ki, int kd) {
+        public PidParameter(int kp, int ki, int kd) {
         super(DataType.PID_PARAMETER);
         this.kp = kp;
         this.ki = ki;
         this.kd = kd;
+    }
+
+    public int getKp() {
+        return kp;
+    }
+
+    public int getKi() {
+        return ki;
+    }
+
+    public int getKd() {
+        return kd;
     }
 }
