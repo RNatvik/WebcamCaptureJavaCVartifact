459266cdccfa08a1b4abc03bab0da7c97f63f945
Lars Berge <lb@stud.ntnu.no>
2019-10-16 16:54:44 +0200

    Testing ipaddress rpi

--- src/main/java/TestMain.java
+++ src/main/java/TestMain.java
@@ -1,8 +1,19 @@
-
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class TestMain {
 
     public static void main(String[] args) {
+        try {
+            InetAddress getLocal = InetAddress.getLocalHost();
+            InetAddress getLoopback = InetAddress.getLoopbackAddress();
+            InetAddress byName = InetAddress.getByName("raspberrypi");
+            System.out.println(getLocal.toString());
+            System.out.println(getLoopback.toString());
+            System.out.println(byName.toString());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
 
     }
 
