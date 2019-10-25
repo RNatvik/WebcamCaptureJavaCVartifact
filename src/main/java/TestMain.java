1014de883df2116b02a569bf43fc778603449187
RNatvik <r_bn-95@hotmail.com>
2019-10-25 10:02:30 +0200

    Reworked TCPClient class. Updated TestMain to test its functionality

--- src/main/java/TestMain.java
+++ src/main/java/TestMain.java
@@ -1,9 +1,15 @@
-import data.Flag;
-import image_processing.Camera;
-import image_processing.ImageProcessor;
+import communication.TCPClientV2;
+import communication.TCPServer;
+import data.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import pub_sub_service.Broker;
+import pub_sub_service.Message;
+import pub_sub_service.Publisher;
+import pub_sub_service.Subscriber;
 
-import java.net.InetAddress;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.net.UnknownHostException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -12,33 +18,170 @@ import java.util.concurrent.TimeUnit;
 public class TestMain {
 
     public static void main(String[] args) {
-        Broker broker = new Broker();
-        Flag flag = new Flag(false);
-        Camera cam = new Camera(0, flag);
-        ImageProcessor processor = new ImageProcessor(flag, broker);
-
-        cam.start();
-        processor.start(cam.getSrcIm());
-
-        ScheduledExecutorService ses = Executors.newScheduledThreadPool(3);
-        ses.scheduleAtFixedRate(cam, 0, 40, TimeUnit.MILLISECONDS);
-        ses.scheduleAtFixedRate(processor, 1000, 10, TimeUnit.MILLISECONDS);
-
+        ScheduledExecutorService ses = Executors.newScheduledThreadPool(5);
         try {
-            Thread.sleep(90000);
+            Broker broker1 = new Broker();
+            Broker broker2 = new Broker();
+            MultiPub publisher = new MultiPub(broker1);
+            Subber subber = new Subber(broker2);
+            TCPServer server = new TCPServer(1234, true, 2, broker1);
+            TCPServer server2 = new TCPServer(1235, true, 2, broker1);
+            TCPClientV2 client = new TCPClientV2("127.0.0.1", 1234, broker2);
+            server2.startThread();
+            server.startThread();
+            Thread.sleep(3000);
+            client.initialize();
+            int counter = 1;
+            boolean finishedSub = false;
+            while (!finishedSub) {
+                boolean success = false;
+                switch (counter) {
+                    case 1:
+                        success = client.setOutputMessage("SUB", Topic.REGULATOR_OUTPUT);
+                        break;
+                    case 2:
+                        success = client.setOutputMessage("SUB", Topic.REGULATOR_PARAM);
+                        break;
+                    case 3:
+                        success = client.setOutputMessage("SUB", Topic.IMPROC_PARAM);
+                        break;
+                    case 4:
+                        success = client.setOutputMessage("SUB", Topic.IMAGE_DATA);
+                        break;
+                    case 5:
+                        success = client.setOutputMessage("SUB", Topic.PID_PARAM1);
+                        break;
+                    case 6:
+                        success = client.setOutputMessage("SUB", Topic.PID_PARAM2);
+                        break;
+                    case 7:
+                        success = client.setOutputMessage("SUB", Topic.CONTROLER_INPUT);
+                        break;
+                    default:
+                        finishedSub = true;
+                        break;
+                }
+                if (success) {
+                    counter += 1;
+                }
+            }
+            ses.scheduleAtFixedRate(broker1, 0, 50, TimeUnit.MILLISECONDS);
+            ses.scheduleAtFixedRate(broker2, 0, 50, TimeUnit.MILLISECONDS);
+            ses.scheduleAtFixedRate(publisher, 1, 500, TimeUnit.MILLISECONDS);
+            ses.scheduleAtFixedRate(subber, 2, 200, TimeUnit.MILLISECONDS);
+
+            client.setHost("127.0.0.1", 1235);
+
+            Thread.sleep(10000);
+
+            client.stopConnection();
+            Thread.sleep(1000);
+            boolean finished = false;
+            while (!finished) {
+                finished = client.isTerminated();
+                System.out.print("");
+            }
+            client.initialize();
+            finished = false;
+            while (!finished) {
+                finished = client.setOutputMessage("SUB", Topic.IMAGE_DATA);
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
         } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
-        cam.stop();
-        processor.stop();
-        ses.shutdown();
-        try {
-            ses.awaitTermination(5, TimeUnit.SECONDS);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    }
+
+    private static class Subber extends Subscriber implements Runnable {
+
+        public Subber(Broker broker) {
+            super(broker);
+            this.getBroker().subscribeTo(Topic.REGULATOR_PARAM, this);
+            this.getBroker().subscribeTo(Topic.REGULATOR_OUTPUT, this);
+            this.getBroker().subscribeTo(Topic.PID_PARAM1, this);
+            this.getBroker().subscribeTo(Topic.PID_PARAM2, this);
+            this.getBroker().subscribeTo(Topic.IMAGE_DATA, this);
+            this.getBroker().subscribeTo(Topic.IMPROC_PARAM, this);
+            this.getBroker().subscribeTo(Topic.CONTROLER_INPUT, this);
+        }
+
+        @Override
+        protected void readMessages() {
+            synchronized (this) {
+                int length = this.getMessageQueue().size();
+                if (length > 0) {
+                    System.out.println("Subber:: " + this.getMessageQueue().size() + " in queue");
+                }
+                while (!this.getMessageQueue().isEmpty()) {
+                    Message message = this.getMessageQueue().remove();
+                }
+            }
+        }
+
+        @Override
+        public void run() {
+            readMessages();
         }
     }
 
+    private static class MultiPub implements Runnable, Publisher {
 
+        private Broker broker;
+
+        public MultiPub(Broker broker) {
+            this.broker = broker;
+        }
+
+        @Override
+        public void run() {
+            ControlInput controlInput = new ControlInput(
+                    true, 10, 20
+            );
+            ImageProcessorData imageProcessorData = new ImageProcessorData(
+                    null, new int[]{1, 2, 3, 4}
+            );
+            ImageProcessorParameter imageProcessorParameter = new ImageProcessorParameter(
+                    1, 2, 3, 4, 5, 6, true
+            );
+            PidParameter pidParameter1 = new PidParameter(
+                    1, 1, 1, 1, 1, 1
+            );
+            PidParameter pidParameter2 = new PidParameter(
+                    2, 2, 2, 2, 2, 2
+            );
+            RegulatorOutput regulatorOutput = new RegulatorOutput(
+                    15, 51
+            );
+            RegulatorParameter regulatorParameter = new RegulatorParameter(
+                    1, 2, 3, 4, 5, 6
+            );
+            Message messageControlInput = new Message(Topic.CONTROLER_INPUT, controlInput);
+            Message messageImProcParam = new Message(Topic.IMPROC_PARAM, imageProcessorParameter);
+            Message messageImProcData = new Message(Topic.IMAGE_DATA, imageProcessorData);
+            Message messagePid1 = new Message(Topic.PID_PARAM1, pidParameter1);
+            Message messagePid2 = new Message(Topic.PID_PARAM2, pidParameter2);
+            Message messageRegOut = new Message(Topic.REGULATOR_OUTPUT, regulatorOutput);
+            Message messageRegParam = new Message(Topic.REGULATOR_PARAM, regulatorParameter);
+            for (int i = 0; i < 1; i++) {
+                this.publish(broker, messageControlInput);
+                this.publish(broker, messageImProcParam);
+                this.publish(broker, messageImProcData);
+                this.publish(broker, messagePid1);
+                this.publish(broker, messagePid2);
+                this.publish(broker, messageRegOut);
+                this.publish(broker, messageRegParam);
+            }
+        }
+
+        @Override
+        public void publish(Broker broker, Message message) {
+            broker.addMessage(message);
+        }
+    }
 }
+
+
--- src/main/java/communication/TCPClient.java
+++ src/main/java/communication/TCPClient.java
@@ -1,8 +1,12 @@
 package communication;
 
-import data.Topic;
+import data.*;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import pub_sub_service.Broker;
+import pub_sub_service.Message;
+import pub_sub_service.Publisher;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,111 +15,245 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
 
-public class TCPClient implements Runnable {
+public class TCPClient implements Runnable, Publisher {
 
     private Thread thread;
+    private InetAddress hostAddress;
+    private int hostPort;
+    private String outputMessage;
     private Socket socket;
-    private PrintWriter printWriter;
-    private BufferedReader bufferedReader;
+    private Broker broker;
+    private boolean initialized;
+    private boolean connected;
     private boolean shutdown;
     private boolean terminated;
 
-    private String outputMessage;
+    public TCPClient(String hostAddress, int hostPort, Broker broker) throws UnknownHostException {
+        this.thread = null;
+        this.hostAddress = InetAddress.getByName(hostAddress);
+        this.hostPort = hostPort;
+        this.outputMessage = null;
+        this.socket = null;
+        this.broker = broker;
+        this.initialized = false;
+        this.connected = false;
+        this.shutdown = false;
+        this.terminated = false;
+    }
 
-    public TCPClient(InetAddress hostAddress, int hostPort) {
-        try {
+    public void initialize() throws IOException {
+        if (!this.initialized) {
             this.thread = new Thread(this);
-            this.socket = new Socket(hostAddress, hostPort);
-            this.printWriter = new PrintWriter(this.socket.getOutputStream());
-            this.bufferedReader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
-            this.shutdown = false;
-            this.terminated = false;
-            this.socket.setSoTimeout(5);
-            this.outputMessage = null;
-        } catch (IOException e) {
-            e.printStackTrace();
+            if (this.connected = this.connect()) {
+                this.socket.setSoTimeout(20);
+                this.shutdown = false;
+                this.terminated = false;
+                this.initialized = true;
+                this.thread.start();
+            } else {
+                throw new IOException(String.format(
+                        "Failed to connect to: %s(%d)",
+                        this.hostAddress, this.hostPort)
+                );
+            }
         }
     }
 
-    public TCPClient(String hostAddress, int hostPort) {
-        try {
-            this.thread = new Thread(this);
-            this.socket = new Socket(hostAddress, hostPort);
-            this.printWriter = new PrintWriter(this.socket.getOutputStream());
-            this.bufferedReader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
-            this.shutdown = false;
-            this.terminated = false;
-            this.socket.setSoTimeout(5);
-            this.outputMessage = null;
-        } catch (IOException e) {
-            e.printStackTrace();
+    public void setHost(String hostAddress, int hostPort) throws UnknownHostException {
+        this.hostAddress = InetAddress.getByName(hostAddress);
+        this.hostPort = hostPort;
+    }
+
+    public synchronized boolean setOutputMessage(String command, String body) {
+        boolean success = false;
+        if (this.outputMessage == null) {
+            this.outputMessage = String.format(command + "::%s", body);
+            success = true;
         }
+        return success;
     }
 
-    public void startThread() {
-        System.out.println(this + ":: starting thread");
-        this.thread.start();
-    }
-
-    public void stop() {
-        System.out.println(this + ":: stop() called");
+    public void stopConnection() {
         this.shutdown = true;
     }
 
+    public boolean isInitialized() {
+        return initialized;
+    }
+
+    public boolean isConnected() {
+        return connected;
+    }
+
     public boolean isTerminated() {
-        return this.terminated;
+        return terminated;
     }
 
-    public void setOutputMessage(String command, String body) {
-        this.outputMessage = String.format(command+"::%s", body);
-        System.out.println(this + ":: set output message: " + this.outputMessage);
-    }
-
-    @Override
-    public void run() {
-        this.printWriter.println(String.format("SUB::%s", Topic.IMAGE_DATA));
-        this.printWriter.flush();
-        while (!this.shutdown) {
-            try {
-                if (this.outputMessage != null) {
-                    this.printWriter.println(this.outputMessage);
-                    this.printWriter.flush();
-                    this.outputMessage = null;
-                }
-                String response = this.bufferedReader.readLine();
-                if (response != null) {
-                    try {
-                        JSONObject jsonObject = new JSONObject(response);
-                        JSONObject data = jsonObject.getJSONObject("data");
-                        String location = data.getJSONArray("location").toString();
-                        System.out.println(this + ":: " + location);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                        System.out.println(this + ":: Error when parsing JSON object.\n   Response: " + response);
-                    }
-                } else {
-                    System.out.println(this + ":: Socket closed remotely");
-                    this.stop();
-                }
-            } catch (SocketTimeoutException e) {
-
-            } catch (IOException e) {
-                e.printStackTrace();
-                this.stop();
-            }
+    private boolean connect() {
+        boolean success = true;
+        try {
+            this.socket = new Socket(this.hostAddress, this.hostPort);
+        } catch (IOException e) {
+            success = false;
+            e.printStackTrace();
         }
-        this.terminated = this.shutdownProcedure();
-        System.out.println(this + ":: terminated: " + this.terminated);
+        return success;
     }
 
     private boolean shutdownProcedure() {
         boolean success = true;
         try {
             this.socket.close();
+            this.initialized = false;
+            this.connected = false;
         } catch (IOException e) {
+            success = false;
             e.printStackTrace();
         }
         return success;
+    }
+
+    private Message parseMessage(String body) {
+        Message message = null;
+        try {
+            JSONObject jsonMessageObject = new JSONObject(body);
+            JSONObject jsonDataObject = jsonMessageObject.getJSONObject("data");
+            String topic = jsonMessageObject.getString("topic");
+            switch (topic) {
+                case Topic.IMPROC_PARAM:
+                    ImageProcessorParameter imProcParam = new ImageProcessorParameter(
+                            jsonDataObject.getInt("hueMin"),
+                            jsonDataObject.getInt("hueMax"),
+                            jsonDataObject.getInt("satMin"),
+                            jsonDataObject.getInt("satMax"),
+                            jsonDataObject.getInt("valMin"),
+                            jsonDataObject.getInt("valMin"),
+                            jsonDataObject.getBoolean("storeProcessedImage")
+                    );
+                    message = new Message(topic, imProcParam);
+                    break;
+
+                case Topic.IMAGE_DATA:
+                    JSONArray location = jsonDataObject.getJSONArray("location");
+                    ImageProcessorData imProcData = new ImageProcessorData(
+                            null,
+                            new int[]{
+                                    location.getInt(0),
+                                    location.getInt(1),
+                                    location.getInt(2),
+                                    location.getInt(3)
+                            }
+                    );
+                    message = new Message(topic, imProcData);
+                    break;
+
+                case Topic.PID_PARAM1:
+                    PidParameter pidParameter1 = new PidParameter(
+                            jsonDataObject.getDouble("kp"),
+                            jsonDataObject.getDouble("ki"),
+                            jsonDataObject.getDouble("kd"),
+                            jsonDataObject.getDouble("maxOutput"),
+                            jsonDataObject.getDouble("minOutput"),
+                            jsonDataObject.getDouble("setpoint")
+                    );
+                    message = new Message(topic, pidParameter1);
+                    break;
+
+                case Topic.PID_PARAM2:
+                    PidParameter pidParameter2 = new PidParameter(
+                            jsonDataObject.getDouble("kp"),
+                            jsonDataObject.getDouble("ki"),
+                            jsonDataObject.getDouble("kd"),
+                            jsonDataObject.getDouble("maxOutput"),
+                            jsonDataObject.getDouble("minOutput"),
+                            jsonDataObject.getDouble("setpoint")
+                    );
+                    message = new Message(topic, pidParameter2);
+                    break;
+
+                case Topic.REGULATOR_OUTPUT:
+                    RegulatorOutput regOutput = new RegulatorOutput(
+                            jsonDataObject.getDouble("leftMotor"),
+                            jsonDataObject.getDouble("rightMotor")
+                    );
+                    message = new Message(topic, regOutput);
+                    break;
+
+                case Topic.REGULATOR_PARAM:
+                    RegulatorParameter regParam = new RegulatorParameter(
+                            jsonDataObject.getDouble("mcMinimumReverse"),
+                            jsonDataObject.getDouble("mcMaximumReverse"),
+                            jsonDataObject.getDouble("mcMinimumForward"),
+                            jsonDataObject.getDouble("mcMaximumForward"),
+                            jsonDataObject.getDouble("controllerMinOutput"),
+                            jsonDataObject.getDouble("controllerMaxOutput")
+                    );
+                    message = new Message(topic, regParam);
+                    break;
+
+                case Topic.CONTROLER_INPUT:
+                    ControlInput controlInput = new ControlInput(
+                            jsonDataObject.getBoolean("manualControl"),
+                            jsonDataObject.getDouble("forwardSpeed"),
+                            jsonDataObject.getDouble("turnSpeed")
+                    );
+                    message = new Message(topic, controlInput);
+                    break;
+
+                default:
+                    break;
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return message;
+    }
+
+    @Override
+    public void run() {
+        BufferedReader bufferedReader = null;
+        PrintWriter printWriter = null;
+        try {
+            bufferedReader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+            printWriter = new PrintWriter(this.socket.getOutputStream());
+        } catch (IOException e) {
+            this.shutdown = true;
+            e.printStackTrace();
+        }
+
+        while (!this.shutdown) {
+            try {
+                if (this.outputMessage != null) {
+                    printWriter.println(this.outputMessage);
+                    printWriter.flush();
+                    this.outputMessage = null;
+                }
+                String body = bufferedReader.readLine();
+                if (body != null) {
+                    Message message = this.parseMessage(body);
+                    this.publish(this.broker, message);
+                } else {
+                    this.shutdown = true;
+                }
+            } catch (SocketTimeoutException e) {
+            } catch (IOException e) {
+                this.shutdown = true;
+                e.printStackTrace();
+            }
+
+        }
+
+        while (!this.terminated) {
+            this.terminated = this.shutdownProcedure();
+        }
+    }
+
+    @Override
+    public void publish(Broker broker, Message message) {
+        if (message != null) {
+            broker.addMessage(message);
+        }
     }
 }
