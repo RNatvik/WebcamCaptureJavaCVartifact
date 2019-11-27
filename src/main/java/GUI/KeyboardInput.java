7b58fc52e7d6cd02aed552f79559ff2f6eef12d8
RNatvik <36402119+RNatvik@users.noreply.github.com>
2019-11-27 12:29:55 +0100

    Update KeyboardInput.java

--- src/main/java/GUI/KeyboardInput.java
+++ src/main/java/GUI/KeyboardInput.java
@@ -2,7 +2,7 @@ package GUI;
 import data.ControlInput;
 import data.GripperControl;
 import javafx.scene.input.KeyEvent;
-
+ 
 /**
  * Handles input from keyboard to manually control the car.
  *
@@ -14,7 +14,7 @@ public class KeyboardInput {
     private String activeKeys;
     // Strings allowed to register.
     private String allowedKeys;
-
+ 
     /**
      * Initialises the class to handle the keyboardinputs
      */
@@ -22,20 +22,21 @@ public class KeyboardInput {
         this.activeKeys = "";
         this.allowedKeys = "ASDWQE";
     }
-
+ 
     /**
+     *  Handles the keyevent and returns a string with the active keys.
      *
-     * Returns Null if there is the KeyEvent is not form an allowed key or if the
-     * @param event Key event.
+     * @param event the Key event fired by the pressing a button when the GUI is active
+     * @return the string containing the active keys on the keyboard
      */
     public String doHandleKeyEvent(KeyEvent event) {
-
+ 
         String keys = null;
-
+ 
         String keyEventType = event.getEventType().toString().toUpperCase();
-
+ 
         String keyName = event.getCode().toString().toUpperCase();
-
+ 
         if (keyEventType.equals("KEY_PRESSED") && !this.activeKeys.contains(keyName) && this.allowedKeys.contains(keyName) ){
             this.activeKeys = this.activeKeys+keyName; //"::"+
             keys = this.activeKeys;
@@ -43,17 +44,22 @@ public class KeyboardInput {
         else if (keyEventType.equals("KEY_RELEASED") && this.activeKeys.contains(keyName) && this.allowedKeys.contains(keyName)){
             this.activeKeys = removeKey(this.activeKeys,keyName);
             keys = this.activeKeys;
-
+ 
         }
-
+ 
         return keys;
     }
-
-
+ 
+    /**
+     * Creates a controlinput based on the active keys
+     *
+     * @param keys the keys to create the controlinput to return
+     * @return the controlinput to control the speed of the motors based on the input
+     */
     public ControlInput getControlInput(String keys) {
         double forwardSpeed = 0;
         double turnSpeed = 0;
-
+ 
         while (keys.contains("W") || keys.contains("A")|| keys.contains("S")|| keys.contains("D")){
             if (keys.contains("W")){
                 forwardSpeed = forwardSpeed + 100;
@@ -74,12 +80,16 @@ public class KeyboardInput {
         }
         return new ControlInput(true,forwardSpeed,turnSpeed);
     }
-
+ 
     private String removeKey(String keys,String keyToRemove) {
         return keys.replace(keyToRemove,"");
     }
-
-
+ 
+    /**
+     * Creates a GripperControl object based on the active keys
+     * @param keys the active keys to create the GripperControl command
+     * @return the GripperControl command based on the input
+     */
     public GripperControl getGripperCommand(String keys) {
         boolean gripper = false;
         while (keys.contains("Q") || keys.contains("E")) {
